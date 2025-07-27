9a1f75f8f28cccd06faf5a9b761a7043e4744863
Vanshika Agarwal <149076293+vanshika250904@users.noreply.github.com>
2025-07-28 01:58:56 +0530

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3005',
+        changeOrigin: true
+      }
+    }
+  },
+  define: {
+    "process.env": { REACT_APP_BASE_URL: 'https://linkease.vercel.app' },
+  }
+});
