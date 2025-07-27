c11c4b50514de9342d99c9467024e486e22fa533
Vanshika Agarwal <149076293+vanshika250904@users.noreply.github.com>
2025-07-28 01:58:28 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,html}',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pri': '#00c2ff'
+      },
+    },
+  },
+  plugins: [],
+};
