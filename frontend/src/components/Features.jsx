2e6438cac8db5e3a81dcc0e7084b76e10b510cd7
Vanshika Agarwal <149076293+vanshika250904@users.noreply.github.com>
2025-07-28 02:11:18 +0530

    Create Features.jsx

---
+++ frontend/src/components/Features.jsx
@@ -0,0 +1,4 @@
+const ErrorMsg = ({ text }) => <p className='mt-4 mx-auto w-4/5 lg:w-2/5
+              text-xl p-1 border-2 break-words text-center border-red-600 rounded bg-slate-200 text-red-600'>{text}</p>;
+
+export default ErrorMsg;
